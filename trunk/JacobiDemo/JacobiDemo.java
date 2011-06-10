9e6c1d4cede37b134be68425da8b880b4e700862
ben@benparetzky.com <ben@benparetzky.com@6679a726-5bc6-a3b4-6cde-791b46f8471e>
2011-06-10 00:24:47 +0000

    Introduced JacobiDemo

---
+++ trunk/JacobiDemo/JacobiDemo.java
@@ -0,0 +1,210 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
+import java.util.Arrays;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * ver.         scruffy.Wed Jun  8 08:53:06 EDT 2011
+ */
+public final class JacobiDemo {
+
+    static final int DEFAULT_WIDTH = 400, DEFAULT_HEIGHT = 200, BORDER = 10, DEFAULT_DELAY = 40;
+    static boolean DEBUG = false;
+    BufferedImage img;
+    int[][] data;
+    int width = DEFAULT_WIDTH, height = DEFAULT_HEIGHT, delay = DEFAULT_DELAY;
+
+    public static void main(String[] args) {
+        new JacobiDemo(args).start();
+    }
+
+    JacobiDemo(String[] args) {
+        parseArgs(args);
+        data = new int[width][height];
+        img = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
+        int[] blue = new int[3];
+        blue[2] = Byte.MAX_VALUE;
+    }
+
+    void start() {
+        JFrame frame = new JFrame("JacobiDemo by Ben Paretzky");
+        frame.setPreferredSize(new Dimension(width + (BORDER << 1) + 10, height + (BORDER << 1) + 45));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(new BorderLayout());
+        JPanel panel = new JPanel() {
+
+            @Override
+            public void paint(Graphics g) {
+                g.drawImage(img, BORDER, BORDER, null);
+            }
+        };
+        panel.addMouseListener(new MouseListener() {
+
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            public void mousePressed(MouseEvent e) {
+		int x, y;
+                x = e.getX() - BORDER - 1;
+                y = e.getY() - BORDER - 1;
+		//System.out.printf("(%d,%d)\n", x,y);
+                try {
+                    data[x][y] = Integer.MAX_VALUE >> 5;
+                } catch (IndexOutOfBoundsException ex) {
+                }
+            }
+
+            public void mouseReleased(MouseEvent e) {
+            }
+
+            public void mouseEntered(MouseEvent e) {
+            }
+
+            public void mouseExited(MouseEvent e) {
+            }
+        });
+        frame.getContentPane().add(panel, BorderLayout.CENTER);
+        frame.pack();
+        frame.setVisible(true);
+        for (;;) {
+            try {
+                Thread.sleep(delay);
+            } catch (InterruptedException ex) {
+            }
+            updateImg();
+            frame.repaint();
+            if (DEBUG) {
+                for (int i = 0; i < width; i++) {
+                    System.out.println(Arrays.toString(data[i]));
+                }
+                System.out.println("");
+            }
+        }
+
+    }
+
+    void updateImg() {
+        WritableRaster r = img.getRaster();
+        int nCount = 0, sum; // Neighbor Count
+        int max = 0;
+        int newVal = 0;
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                nCount = sum = newVal = 0;
+                if (i != 0) {
+                    nCount++;
+                    sum += data[i - 1][j];
+                }
+                if (j != height - 1) {
+                    nCount++;
+                    sum += data[i][j + 1];
+                }
+                if (i != width - 1) {
+                    nCount++;
+                    sum += data[i + 1][j];
+                }
+                if (j != 0) {
+                    nCount++;
+                    sum += data[i][j - 1];
+                }
+                sum += data[i][j];
+                nCount++;
+                newVal = sum / nCount;
+                if (newVal > max) {
+                    max = newVal;
+                }
+                if (newVal != data[i][j]) {
+                    data[i][j] = newVal;
+                }
+            }
+        }
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                r.setPixel(i, j, makeBGR(data[i][j], max));
+            }
+        }
+    }
+
+    int[] makeBGR(int b, int max) {
+        float percent = (max == 0) ? 0 : ((float) b) / max;
+        int[] c = new int[3];
+        c[0] = (int) (percent * Byte.MAX_VALUE);
+        c[2] = Byte.MAX_VALUE - c[0];
+        return c;
+    }
+
+    void parseArgs(String[] args) {
+        int i = 0;
+        for (String s : args) {
+            if (s.startsWith("-w")) {
+                s = s.substring(2);
+                try {
+                    i = Integer.parseInt(s);
+                } catch (NumberFormatException ex) {
+                    continue;
+                }
+                if (i > 0) {
+                    width = i;
+                    continue;
+                } else {
+                    System.out.println("Invalid width");
+                    System.exit(1);
+                }
+            }
+            if (s.startsWith("-h") && !s.equals("-help")) {
+                s = s.substring(2);
+                try {
+                    i = Integer.parseInt(s);
+                } catch (NumberFormatException ex) {
+                    continue;
+                }
+                if (i > 0) {
+                    height = i;
+                    continue;
+                } else {
+                    System.out.println("Invalid height");
+                    System.exit(1);
+                }
+            }
+            if (s.startsWith("-d")) {
+                s = s.substring(2);
+                try {
+                    i = Integer.parseInt(s);
+                } catch (NumberFormatException ex) {
+                    if (s.equals("ebug")) {
+                        DEBUG = true;
+                    }
+                    continue;
+                }
+                if (i >= 0) {
+                    delay = i;
+                    continue;
+                } else {
+                    System.out.println("Invalid delay");
+                    System.exit(1);
+                }
+            }
+            if (s.equals("-help") || s.equals("--help")) {
+                printHelp();
+                System.exit(0);
+            }
+        }
+    }
+
+    static void printHelp() {
+        final String help =
+                "Jacobi Relaxation Demo by Ben Paretzky <ben@benparetzky.com> (6/7/11)\n"
+                + "Arguments:\n\t-wN\tWidth, N number of pixels.  Default: " + DEFAULT_WIDTH + "\n"
+                + "\t-hN\tHeight, N number of pixels.  Default: " + DEFAULT_HEIGHT + "\n"
+                + "\t-dN\tDelay, sleep ~N milliseconds inbetween painting.  Default: " + DEFAULT_DELAY + "\n";
+        System.out.println(help);
+    }
+}
