e9133c47286f31dcb02464b096ed897ab18c6374
kevin.broberg <kevin.broberg@6679a726-5bc6-a3b4-6cde-791b46f8471e>
2011-06-17 13:46:51 +0000

    Halfway implemented task messages

--- trunk/JacobiDemo/src/hpfs/JacobiServer.java
+++ trunk/JacobiDemo/src/hpfs/JacobiServer.java
@@ -1,5 +1,6 @@
 package hpfs;
 
+import java.awt.image.WritableRaster;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -8,9 +9,13 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.concurrent.RecursiveTask;
 
 /**
  *
@@ -140,5 +145,195 @@ public class JacobiServer {
             in = new ObjectInputStream(s.getInputStream());
         }
     }
+    
+    // copied from JacobiDemoFJ
+    
+    class LeafNode extends Node {
 
+        final JacobiDemoFJ demo;
+        int max = Integer.MIN_VALUE;
+
+        LeafNode(JacobiDemoFJ demo, int left, int top, int right, int bottom) {
+            super(left, top, right, bottom);
+            this.demo = demo;
+        }
+
+        int stepPoint(int[][] from, int[][] to, int w, int h) {
+            int l, r, u, d, nCount; // left, right, up, down
+            if (w != 0) {
+                l = 1;
+            } else {
+                l = 0;
+            }
+            if (h != 0) {
+                u = 1;
+            } else {
+                u = 0;
+            }
+            if (w + 1 < demo.width) {
+                r = 1;
+            } else {
+                r = 0;
+            }
+            if (h + 1 < demo.height) {
+                d = 1;
+            } else {
+                d = 0;
+            }
+            nCount = l + u + r + d;
+            double runVal = 0d;
+            if (l == 1) {
+                runVal += from[w - 1][h];
+            }
+            if (u == 1) {
+                runVal += from[w][h - 1];
+            }
+            if (r == 1) {
+                runVal += from[w + 1][h];
+            }
+            if (d == 1) {
+                runVal += from[w][h + 1];
+            }
+            runVal += from[w][h];
+            nCount++;
+            to[w][h] = (int) (runVal / nCount);
+            return to[w][h];
+        }
+
+        @Override
+        protected Integer compute() {
+            int[][] prev = demo.getPreviousData();
+            int[][] cur = demo.getCurrentData();
+            int retVal = Integer.MIN_VALUE, i;
+            for (int c = left; c < right; c++) {
+                for (int r = top; r < bottom; r++) {
+                    i = stepPoint(prev, cur, c, r);
+                    if (i > retVal) {
+                        retVal = i;
+                    }
+                }
+            }
+            return max = retVal;
+        }
+
+        @Override
+        int getMax() {
+            return max;
+        }
+    }
+
+    class InnerNode extends Node {
+
+        final Collection<Node> children;
+        int max = Integer.MIN_VALUE;
+
+        InnerNode(int left, int top, int right, int bottom) {
+            super(left, top, right, bottom);
+            children = new ArrayList(4);
+            int wMid = (left + right) / 2;
+            if (wMid == left) {
+                wMid = right;
+            }
+            int hMid = (top + bottom) / 2;
+            if (hMid == top) {
+                hMid = bottom;
+            }
+            if ((right - left) * (bottom - top) > THREASHOLD) {
+                children.add(new InnerNode(left, top, wMid, hMid));
+                children.add(new InnerNode(wMid, top, right, hMid));
+                children.add(new InnerNode(left, hMid, wMid, bottom));
+                children.add(new InnerNode(wMid, hMid, right, bottom));
+            } else {
+                children.add(new LeafNode(left, top, wMid, hMid));
+                children.add(new LeafNode(wMid, top, right, hMid));
+                children.add(new LeafNode(left, hMid, wMid, bottom));
+                children.add(new LeafNode(wMid, hMid, right, bottom));
+            }
+        }
+
+        @Override
+        protected Integer compute() {
+            int retVal = Integer.MIN_VALUE;
+            //invokeAll(children);
+            for (Node n : invokeAll(children)) {
+                if (n.getMax() > retVal) {
+                    retVal = n.getMax();
+                }
+                n.reinitialize();
+            }
+            return max = retVal;
+        }
+
+        @Override
+        int getMax() {
+            return max;
+        }
+    }
+
+    abstract class Node extends RecursiveTask<Integer> {
+
+        final int left, top, right, bottom;
+
+        Node(int left, int top, int right, int bottom) {
+            this.left = left;
+            this.top = top;
+            this.right = right;
+            this.bottom = bottom;
+        }
+
+        @Override
+        protected abstract Integer compute();
+
+        abstract int getMax();
+    }
+    
+    // end copied classes for Jacobi computations
+    
+    private static class nextFrameTask extends RecursiveTask {
+        private int[][] cur, prev;
+        private int startx, starty, width, height;
+        
+        public nextFrameTask( int[][] frame, int startx, int starty, int width, int height ) {
+            this.cur = frame;
+            this.prev = frame;
+            this.startx = startx;
+            this.starty = starty;
+            this.width = width;
+            this.height = height;
+        }
+        
+        @Override
+        protected Object compute() {
+            // compute next frame of Jacobi; champ = max value in new array
+            int[][] temp = cur;
+            cur = prev;
+            prev = temp;
+        
+            
+            return max;
+        }
+        
+        
+        /**
+         * Given a number and a max from a set, returns int [] BGR value from
+         * blue to red.  b/max == 1 for Red, and b == 0 for Blue
+         * @param b Value
+         * @param max Maximum
+         * @return BGR array. All values will sum to where 0 <= (b / max)  <= 1
+         */
+        int[] makeBGR(int b, int max) {
+            float percent = (max == 0) ? 0 : ((float) b) / max;
+            int[] c = new int[3];
+            c[0] = (int) (percent * Byte.MAX_VALUE);
+            c[2] = Byte.MAX_VALUE - c[0];
+            return c;
+        }
+    }
+    
+    private static class renderImageTask extends RecursiveTask {
+        @Override
+        protected Object compute() {
+            return this;
+        }
+    }
 }
